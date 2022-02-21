4831e2d2220bbe9456f9f3c1611f7347039e8aa6
Alejandro Rubio <86860091+AlejandroRubioB@users.noreply.github.com>
2022-02-21 15:56:06 -0600

    Update Vision artificial para el analisis.py

--- Vision artificial para el analisis.py
+++ Vision artificial para el analisis.py
@@ -1,9 +1,10 @@
+from platform import architecture
 import cv2
 import numpy as np
 import imutils
 import time
 import datetime
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import os
 #Este es el video que se va a analizar pero se puede ingresar una direccion Ip
 # Ejemplo: cap = cv2.VideoCapture('http://......)
@@ -16,18 +17,22 @@ car_counter = 0
 car_counter2 = 0
 car_counter3 = 0
 #Dar de alta el GPIO
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(40, GPIO.OUT)
-GPIO.output(40, False)
-GPIO.setup(7, GPIO.OUT)
-GPIO.output(7, False)
-GPIO.setup(11, GPIO.OUT)
-GPIO.output(11, False)
-GPIO.setup(32, GPIO.OUT)
-GPIO.output(32, False)
+#coment GPIO lines
+
+###
+# 
+#GPIO.setwarnings(False)
+#GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(40, GPIO.OUT)
+#GPIO.output(40, False)
+#GPIO.setup(7, GPIO.OUT)
+#GPIO.output(7, False)
+#GPIO.setup(11, GPIO.OUT)
+#GPIO.output(11, False)
+#GPIO.setup(32, GPIO.OUT)
+#GPIO.output(32, False)
 while True:
-    GPIO.output(32, True)
+  #  GPIO.output(32, True)
     #Obtener la fecha y hora actual
     ahora = datetime.datetime.now()
     dia = str(ahora.day)
@@ -81,16 +86,22 @@ while True:
     cnts2= cv2.findContours(fgmask2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
     cnts3= cv2.findContours(fgmask3, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 
-    #Obtener la hora y ponerla en formato de string en este caso es "Hora y minuto"
-    timee = time.strftime("%H-%M")
-    horap = str(timee)
+    #Obtener la FECHA y ponerla en formato de string en este caso es "DIA MES Y AÑO"
+    arch = time.strftime("%d-%m-%Y")
+    fecha = str(arch)
     #Agregar al string el fomate deseado en este caso .txt
-    nombres = horap + ".txt"
+    calle1 = "calle1-" + fecha + ".txt"
     #Verificar si existe el archivo y en caso de que no exista lo crea
-    if not os.path.exists(nombres):
-        f= open(nombres,"w+")
-
-
+    if not os.path.exists(calle1):
+        f= open(calle1,"w+")
+    calle2 = "calle2-" + fecha + ".txt"
+    #Verificar si existe el archivo y en caso de que no exista lo crea
+    if not os.path.exists(calle2):
+        f= open(calle2,"w+")
+    calle3 = "calle3-" + fecha + ".txt"
+    #Verificar si existe el archivo y en caso de que no exista lo crea
+    if not os.path.exists(calle3):
+        f= open(calle3,"w+")
     #Verificar si existe contorno y en caso de que si lo recorre        
     for cnt in cnts:
         #Obtener el area del contorno y verificar su tamaño 
@@ -98,11 +109,11 @@ while True:
             x, y, w, h = cv2.boundingRect(cnt)
             cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,255), 1)
             if 60 < (x + w ) < 80:
-                GPIO.output(40, True)
+                #GPIO.output(40, True)
                 #Agregar el contador de carros
                 car_counter = car_counter + 1
                 cv2.line(frame, (70, 220), (70, 255), (0, 255, 0), 3)
-                archivo = open(nombres,"a")
+                archivo = open(calle1,"a")
                 carro = "1"
                 #Mandar la fecha y hora al archivo 
                 #Se decidio mandar "," por si es necesario hacer un archivo .csv y obtener el formato deseado en Excel
@@ -126,19 +137,19 @@ while True:
                 archivo.write(",")
                 archivo.write("\n")
                 archivo.close()
-                GPIO.output(40, False)
+                #GPIO.output(40, False)
                 
     for cnt2 in cnts2:
         if cv2.contourArea(cnt2) > 1500:
             x, y, w, h = cv2.boundingRect(cnt2)
             cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 1)
             if 385 < (x + w) < 405:
-                GPIO.output(7, True)
+                #GPIO.output(7, True)
                 car_counter2 = car_counter2 + 1
                 cv2.line(frame, (395, 275), (395, 310), (0, 255, 250), 3)
-                archivo = open(nombres,"a")
+                archivo = open(calle2,"a")
                 carro2 = "1"
-                archivo.write(",")              
+                #archivo.write(",")              
                 archivo.write(carro2)
                 archivo.write(",")
                 archivo.write(",")
@@ -158,20 +169,20 @@ while True:
                 archivo.write(",")
                 archivo.write("\n")
                 archivo.close()
-                GPIO.output(7, False)
+                #GPIO.output(7, False)
 
     for cnt3 in cnts3:
         if cv2.contourArea(cnt3) > 1500:
             x, y, w, h = cv2.boundingRect(cnt3)
             cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,255), 1)
             if 215 < (x + w) < 235:
-                GPIO.output(11, True)
+                #GPIO.output(11, True)
                 car_counter3 = car_counter3 + 1
                 cv2.line(frame, (225, 61), (225, 96), (0, 255, 150), 3)
-                archivo = open(nombres,"a")
+                archivo = open(calle3,"a")
                 carro3 = "1"
-                archivo.write(",")
-                archivo.write(",")
+                #archivo.write(",")
+                #archivo.write(",")
                 archivo.write(carro3)
                 archivo.write(",")
                 archivo.write(",")
@@ -190,7 +201,7 @@ while True:
                 archivo.write(",")
                 archivo.write("\n")
                 archivo.close()
-                GPIO.output(11, False)
+                #GPIO.output(11, False)
             
 
 
@@ -227,6 +238,6 @@ while True:
     k = cv2.waitKey(1) & 0xFF
     if k ==27:
         break
-GPIO.output(32, False)
+#GPIO.output(32, False)
 cap.release()
 cv2.destroyAllWindows()
